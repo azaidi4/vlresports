c39ae3e41bbf1c0b9fa2dcdef31f729312dd4d6f
Ahmad Zaidi <azaidi4@wisc.edu>
2025-02-17 01:27:46 +0530

    feat: use node-html-parser for `/events`

--- src/services/eventsService.ts
+++ src/services/eventsService.ts
@@ -1,79 +1,58 @@
-import { fromURL } from 'cheerio';
+import { parse } from 'node-html-parser';
+import { eventSelectors } from '../utils/selectors';
 
-/**
- * Retrieves and parses event data based on the provided status, region, and page number from a specified URL using cheerio and fetch modules.
- * @param {string} status - The status of the events to filter ("all", "upcoming", "live", "completed").
- * @param {string} region - The region of the events to filter (e.g., "na", "eu").
- * @param {number} page - The page number of the events to retrieve (default is 1).
- * @returns {Object} An object containing event details including event name, status, prize pool, dates, region, and event image URL.
- */
-export async function getEvents(status, region, page) {
-  // Send a request to the specified URL with the provided region and page number, then parse the HTML response using cheerio
+export async function getEvents(status: string, region: string, page: number) {
+  const url = `${process.env.VLR_URL}/events/${region}?page=${page}`;
+  const res = await fetch(url);
+  const html = await res.text();
 
-  // Array to store event objects
-  const $ = await fromURL(`${process.env.VLR_URL}/events/${region}?page=${page}`);
-  const events = [];
+  let rootSelector;
 
-  // Iterate over each event item on the page and extract relevant information
-  $('.event-item').each((index, element) => {
-    // Extract event ID, name, and status
-    const href = $(element).attr('href');
-    const id = href.match(/\/event\/(\d+)\//)[1];
-    const name = $(element).find('.event-item-title').text().trim();
-    const eventStatus = $(element)
-      .find('.event-item-desc-item-status')
-      .text()
-      .trim();
+  switch (status) {
+    case 'completed':
+      rootSelector = eventSelectors.root.completed;
+      break;
+    case 'ongoing':
+    case 'upcoming':
+      rootSelector = eventSelectors.root.upcoming;
+      break;
+    default:
+      rootSelector = eventSelectors.root.all;
+  }
 
-    // Check if the event status matches the provided status or if status is "all"
-    if (status === 'all' || eventStatus === status) {
-      // Extract prize pool, dates, country, and event image URL
-      const prizepoolText = $(element)
-        .find('.event-item-desc-item.mod-prize')
-        .text()
-        .trim();
-      const prizepool = prizepoolText.replace(/\D/g, '');
-      const datesText = $(element)
-        .find('.event-item-desc-item.mod-dates')
-        .text()
-        .trim();
-      const dates = datesText
-        .match(/[a-zA-Z]+|\d+/g)
-        .join(' ')
-        .replace(' Dates', '');
-      const country = $(element)
-        .find('.event-item-desc-item.mod-location .flag')
-        .attr('class')
-        .split(' ')[1]
-        .replace('mod-', '');
-      const img = $(element)
-        .find('.event-item-thumb img')
-        .attr('src')
-        .includes('/img/vlr')
-        ? process.env.VLR_URL +
-          $(element).find('.event-item-thumb img').attr('src')
-        : 'https:' + $(element).find('.event-item-thumb img').attr('src');
+  return parse(html)
+    .querySelectorAll(rootSelector)
+    .map((eventItem) => {
+      const dates = eventItem
+        .querySelector(eventSelectors.dates)
+        ?.firstChild?.text.trim()
+        .split('—');
 
-      // Create event object and push it to the events array
-      events.push({
-        id,
-        name,
-        status: eventStatus,
-        prizepool,
-        dates,
-        country,
-        img,
-      });
-    }
-  });
+      const countryName = eventItem
+        .querySelector(eventSelectors.country.name)
+        ?.classNames.split('-')[1];
+      const countryImg = `https://www.vlr.gg/img/icons/flags/16/${countryName}.png`;
 
-  // Return an object containing the list of events and the number of events in the list
-  return {
-    events: events,
-    size: events.length,
-  };
-}
+      const url = eventItem
+        .querySelector(eventSelectors.img)
+        ?.getAttribute('src');
 
-async function getEventById(id) {
-  /* TODO */
+      return {
+        id: eventItem.getAttribute('href')?.split('/')[2],
+        name: eventItem.querySelector(eventSelectors.name)?.text.trim(),
+        status: eventItem.querySelector(eventSelectors.status)?.text.trim(),
+        prizePool: eventItem
+          .querySelector(eventSelectors.prizePool)
+          ?.firstChild?.text.trim(),
+        dates: {
+          start: dates ? dates[0] : null,
+          end: dates ? dates[1] : null,
+        },
+        country: {
+          name: countryName,
+          img: countryImg,
+        },
+        img: url?.includes('vlr') ? process.env.VLR_URL + url : 'https:' + url,
+      };
+    });
 }
--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -14,3 +14,19 @@ export const matchSelectors = {
   },
   time: '.match-item-time',
 };
+
+export const eventSelectors = {
+  root: {
+    all: '.events-container-col .event-item',
+    upcoming: '.events-container-col:has(.mod-upcoming) .event-item',
+    completed: '.events-container-col:has(.mod-completed) .event-item',
+  },
+  name: '.event-item-title',
+  status: '.event-item-desc-item-status',
+  prizePool: '.mod-prize',
+  dates: '.mod-dates',
+  country: {
+    name: '.flag',
+  },
+  img: '.event-item-thumb img'
+};
