0a2a3a46847bf78f44c74e5a0775bac2ff5ebfe9
Ahmad Zaidi <azaidi4@wisc.edu>
2025-03-06 18:00:19 +0530

    feat: import style linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,20 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+// @ts-ignore
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic
-);
+export default tseslint.config({
+  files: ['**/*.{ts,tsx}'],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+    importPlugin.flatConfigs.recommended,
+    importPlugin.flatConfigs.typescript,
+  ],
+  rules: {
+    'import/order': 'error',
+  },
+  // other configs...
+});
